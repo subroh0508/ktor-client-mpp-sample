fc42aca69e77656dc2c0ff46006d09127ba437a6
subroh0508 <subroh.0508@gmail.com>
2021-01-16 12:59:40 +0900

    [edit]Add SearchByLiveViewModel injection

--- android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/ColorMasterApplication.kt
+++ android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/ColorMasterApplication.kt
@@ -3,7 +3,9 @@ package net.subroh0508.colormaster.androidapp
 import android.app.Application
 import net.subroh0508.colormaster.components.core.AppModule
 import net.subroh0508.colormaster.presentation.preview.viewmodel.PreviewViewModel
+import net.subroh0508.colormaster.presentation.search.viewmodel.SearchByLiveViewModel
 import net.subroh0508.colormaster.presentation.search.viewmodel.SearchByNameViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -14,8 +16,9 @@ class ColorMasterApplication : Application() {
 
         startKoin {
             modules(AppModule + AppPreferenceModule + module {
-                single<Application> { this@ColorMasterApplication }
+                androidContext(this@ColorMasterApplication)
                 viewModel { SearchByNameViewModel(get()) }
+                viewModel { SearchByLiveViewModel(get(), get()) }
                 viewModel { PreviewViewModel(get()) }
             })
         }
